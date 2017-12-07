766b6bbab9db7468a7d83af91d84b4f4b34bb2c1
Brian Whitton <me@brianwhitton.com>
2017-12-07 07:00:39 -0500

    wip results from demo index

--- src/App.js
+++ src/App.js
@@ -1,10 +1,83 @@
 import React, { Component } from 'react';
+import {
+  InstantSearch,
+  SearchBox,
+  Hits,
+  Highlight,
+  Stats,
+  Pagination,
+  CurrentRefinements,
+} from 'react-instantsearch/dom';
+
+import { connectRefinementList } from 'react-instantsearch/connectors';
+
+// const APP_ID = "CUET2HJEQ6"
+// const API_KEY = "6a6741dff7b23ea76f8749c6cc16b38b"
+// const INDEX_NAME = "measure"
+const APP_ID = "latency"
+const API_KEY = "3d9875e51fbd20c7754e65422f7ce5e1"
+const INDEX_NAME = "bestbuy"
+
+const RefinementList = connectRefinementList(function({refine, currentRefinement, items}) {
+  let list = items.map((item, i) => {
+    return <li key={i}>{item.isRefined ? 'active' : ''}<button onClick={() => refine(item.value)}>{item.label}</button></li>
+  });
+  return <ul>{list}</ul>
+});
+
+const Hit = ({hit}) =>
+  <div className="hit">
+    <div className="hit-image"><img src={hit.image} /></div>
+    <div className="hit-content">
+      <div className="hit-price">
+        ${hit.salePrice}
+      </div>
+      <div className="hit-name">
+        <Highlight attributeName="name" hit={hit} />
+      </div>
+      <div className="hit-descriptoin">
+        <Highlight attributeName="shortDescription" hit={hit} />
+      </div>
+    </div>
+  </div>
+
+const Sidebar = () =>
+  <div className="sidebar">
+    <h5>Category</h5>
+    <RefinementList attributeName="category" />
+    <h5>Manufacturer</h5>
+    <RefinementList attributeName="manufacturer" operator="and" />
+  </div>
+  
+const Content = () =>
+  <div className="content">
+    <div className="info">
+      <Stats />
+    </div>
+    <Hits hitComponent={Hit} />
+    <div className="pagination">
+      <Pagination showLast />
+    </div>
+  </div>
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <InstantSearch
+        appId={APP_ID}
+        apiKey={API_KEY}
+        indexName={INDEX_NAME}>
+        
+        <header>
+          <SearchBox translations={{placeholder: 'Search'}} />
+        </header>
+        
+        <main>
+          <Sidebar />
+          <Content />
+        </main>
+        
+      </InstantSearch>
     );
   }
 }
