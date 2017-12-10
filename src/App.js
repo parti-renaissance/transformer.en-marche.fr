1575ce32ed6c88239f6708dcee47eee086022a9e
Brian Whitton <me@brianwhitton.com>
2017-12-10 01:38:21 -0500

    moving results wrapper into its own module

--- src/App.js
+++ src/App.js
@@ -3,41 +3,18 @@ import {
   InstantSearch,
   Hits,
 } from 'react-instantsearch/dom';
-import { connectStateResults } from 'react-instantsearch/connectors';
 import './scss/App.css';
 
 import Page from './js/components/Page';
 import Sidebar from './js/static/sidebar';
 import Theme from './js/static/theme';
+import Results from './js/static/results';
 
 const APP_ID = process.env.REACT_APP_ALGOLIA_APP_ID;
 const API_KEY = process.env.REACT_APP_ALGOLIA_API_KEY;
 const INDEX_NAME = process.env.REACT_APP_ALGOLIA_INDEX_NAME;
 
-const Intro = () =>
-  <div className="intro">
-    <h1 className="intro-header">Alors, ça avance ?</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. aliquam erat volutpat. aliquam erat volutpat. aliquam.
-    </p>
 
-    <p>
-      Cette interface sera mise à jour régulièrement. Pour ne rien manquer, recevez notre newsletter hebdomadaire.
-    </p>
-  </div>
-
-const Results = connectStateResults(({searchState, searchResults, props}) => {
-  if (searchState.menu && searchState.menu['profiles.profile.title'].length) {
-    return props.children;
-  } else {
-    return (
-      <div>
-        <Intro />
-        {props.children}
-      </div>
-    );
-  }
-});
 
 const Content = () =>
   <div className="content">
