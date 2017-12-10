7d71ebbccb82aeb44941eb516d81116fb844bd5f
Brian Whitton <me@brianwhitton.com>
2017-12-10 01:41:03 -0500

    refactor LastUpdated
    the passed in props will be raw list of hits. so there's no need to use
    a new Index. That approach wasn't working anyway.

--- src/js/static/last-updated.js
+++ src/js/static/last-updated.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { Index } from 'react-instantsearch/dom';
-import { connectHits } from 'react-instantsearch/connectors';
-import { map, sortBy } from 'lodash';
+import { map } from 'lodash';
 import moment from 'moment-timezone';
 
 moment.tz.setDefault('Europe/Paris');
 
-const MostRecentMeausure = connectHits(({ hits }) => {
-  let dates = map(hits, 'updatedAt');
+const LastUpdated = ({ measures }) => {
+  let dates = map(measures, 'updatedAt');
   dates.sort();
   let diff = moment().diff(dates[0], 'days');
   return <small>Dernière mise à jour il y a {diff} jours.</small>
-});
+};
 
-export default () =>
-  <Index indexName="Measure_dev">
-    <MostRecentMeausure />
-  </Index>
+export default LastUpdated;
