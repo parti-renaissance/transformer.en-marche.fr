1c7c8f6b10b26bb7d1fbb0ed3aefc36a0f945fc2
Brian Whitton <me@brianwhitton.com>
2017-12-13 22:58:33 -0500

    The <Measures /> element return a list or null after filtering
    follow suit. return null instead of the theme markup

--- src/js/static/theme.js
+++ src/js/static/theme.js
@@ -3,17 +3,29 @@ import { Measures, NoMeasure } from './measure';
 
 import './../../scss/theme.css';
   
-const Theme = ({hit:theme}) =>
-  <article className="theme">
-    <img src={theme.image} className="theme-image" alt={theme.title} />
-    
-    <h1 className="theme-title">{theme.title}</h1>
-    
-    <p className="theme-body">{theme.description}</p>
-      
+const Theme = ({hit:theme}) => {
+  let measures = (
     <Measures measures={theme.measures}>
       <NoMeasure theme={theme.title} />
     </Measures>
-  </article>
+  );
+  
+  if (!measures) {
+    return null;
+  }
+  
+  return (
+    <article className="theme">
+      <img src={theme.image} className="theme-image" alt={theme.title} />
+      
+      <h1 className="theme-title">{theme.title}</h1>
+      
+      <p className="theme-body">{theme.description}</p>
+      
+      {measures}
+        
+    </article>
+  )
+}
 
 export default Theme;
