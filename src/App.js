35b0edd3cfe819774bcc2155fee8be7d180ec082
Brian Whitton <me@brianwhitton.com>
2017-12-19 02:37:30 -0500

    sync url with searchState using actions

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Page from './js/components/Page';
 import Sidebar from './js/static/sidebar';
 import Results from './js/static/results';
 
+import { setProfile, toggleThemeFacet } from './js/actions/search-actions';
+
 const APP_ID = process.env.REACT_APP_ALGOLIA_APP_ID;
 const API_KEY = process.env.REACT_APP_ALGOLIA_API_KEY;
 const INDEX_NAME = process.env.REACT_APP_ALGOLIA_INDEX_NAME;
@@ -19,12 +21,37 @@ const Content = () =>
   </div>
   
 class Layout extends Component {
+  
+  syncForProfile() {
+    let { profiles, match: { params }, dispatch, searchState } = this.props;
+    if (!profiles.items.length) {
+      return;
+    }
+    let profile = find(profiles.profiles, ['slug', params.profile])
+    if (profile && !Object.keys(searchState.menu).length) {
+      dispatch(setProfile(profile.objectID));
     }
   }
   
+  syncForTheme() {
+    let { themes, location, dispatch, searchState } = this.props;
+    let { theme } = qs.parse(location.search.slice(1));
+    if (!themes.items.length || !theme) {
+      return;
+    }
+    
+    let foundThemes = filter(themes.themes, t => theme.split(',').includes(t.slug));
+    
+    if (theme && !searchState.refinementList.title.length) {
+      foundThemes.forEach(({ objectID }) => dispatch(toggleThemeFacet(objectID)));
+    }
   }
   
+  componentDidUpdate(prevProps) {
+    this.syncForProfile();
+    this.syncForTheme();
   }
+  
   render() {
     let { measures, themes } = this.props;
     return (
