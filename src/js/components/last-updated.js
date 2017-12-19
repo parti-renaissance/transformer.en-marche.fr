d8313dee7f2c8810b12c869d642d1b0d5d6d518d
Brian Whitton <me@brianwhitton.com>
2017-12-19 13:06:52 -0500

    accept a classname in last updated

--- src/js/components/last-updated.js
+++ src/js/components/last-updated.js
@@ -3,7 +3,7 @@ import { map } from 'lodash';
 import moment from 'moment-timezone';
 import { connect } from 'react-redux';
 
-const LastUpdated = ({ measures }) => {
+const LastUpdated = ({ measures: { measures }, className }) => {
   let dates = map(measures, 'updatedAt');
   dates.sort();
   let diff = moment().diff(dates[0], 'days');
@@ -12,7 +12,7 @@ const LastUpdated = ({ measures }) => {
     diff = moment().diff(dates[0], 'hours');
     unit = 'hours';
   }
-  return <small>Dernière mise à jour il y a {diff} {unit}.</small>
+  return <small className={className}>Dernière mise à jour il y a {diff} {unit}.</small>
 };
 
-export default connect(state => ({measures: state.measures.items}))(LastUpdated);
+export default connect(state => ({measures: state.measures}))(LastUpdated);
