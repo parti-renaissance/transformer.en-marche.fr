5f25acd434eeca0ec9c176a34a845ccfbe666d08
Brian Whitton <me@brianwhitton.com>
2017-12-19 02:38:52 -0500

    intialize with store and provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import { fetchIndexes } from './js/actions/load-data';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './js/store';
+
+store.dispatch(fetchIndexes());
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
