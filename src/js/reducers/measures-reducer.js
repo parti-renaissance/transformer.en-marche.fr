e800a47e615c4220b428fdc93a578b71688de4aa
Brian Whitton <me@brianwhitton.com>
2017-12-19 22:12:54 -0500

    updates measures reducer
    merge in counts from votes action

--- src/js/reducers/measures-reducer.js
+++ src/js/reducers/measures-reducer.js
@@ -12,9 +12,11 @@ export default function measuresReducer(state = {
   switch(action.type) {
     case `${INDEXES}_PENDING`:
       return {...state, fetching: true};
+
     case `${INDEXES}_REJECTED`:
       return {...state, fetching: false, error: action.payload};
-    case `${INDEXES}_FULFILLED`:
+
+    case `${INDEXES}_FULFILLED`: {
       let { measures } = action.payload;
       const newState = {
         ...state,
@@ -22,8 +24,22 @@ export default function measuresReducer(state = {
         fetched: true,
         items: measures.map(measure => measure.objectID),
       };
-      measures.forEach(measure => newState.measures[measure.objectID] = measure);
+      measures.forEach(measure => {
+        let stateMeasure = newState.measures[measure.objectID];
+        newState.measures[measure.objectID] = Object.assign({}, stateMeasure, measure);
+      });
       return newState;
+    }
+    
+    case `${VOTES}_FULFILLED`: {
+      const newState = { ...state };
+      action.payload.forEach(({ itemId, count }) => {
+        let stateMeasure = newState.measures[itemId] || {};
+        newState.measures[itemId] = Object.assign({}, stateMeasure, {count});
+      });
+      return newState;
+    }
+
     default:
       return state;
   }
