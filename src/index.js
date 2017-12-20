56c3d12f80caf17257152243a4c55e14be58c25d
Brian Whitton <me@brianwhitton.com>
2017-12-20 00:49:13 -0500

    check query string at loade for a code param
    get an access token if there's one

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import moment from 'moment-timezone';
+import qs from 'qs';
 
 import './index.css';
 import App from './App';
 import { fetchIndexes } from './js/actions/data-actions';
+import { getToken } from './js/actions/auth-actions';
 import store from './js/store';
 
 moment.tz.setDefault('Europe/Paris');
 
 store.dispatch(fetchIndexes());
+
+let queryParams = qs.parse(window.location.search.slice(1));
+
+if (queryParams.code) {
+  store.dispatch(getToken(queryParams.code))
+}
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
