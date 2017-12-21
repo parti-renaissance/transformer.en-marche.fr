36f861a1ae8d883b02fbe3ae5c4411319391a335
Brian Whitton <me@brianwhitton.com>
2017-12-20 23:16:26 -0500

    this was getting in the way. going to reimpliment

--- src/js/store.js
+++ src/js/store.js
@@ -6,23 +6,10 @@ import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
 import createHistory from 'history/createBrowserHistory';
 
-import { openAuth } from './actions/auth-actions';
-
 import { loadState } from './local-storage';
 import reducer from './reducers';
 
 export const history = createHistory();
-
-const authMiddleware = (authActions = []) => {
-  return store => next => action => {
-    let state = store.getState();
-    if (authActions.includes(action.type) && !state.auth.token) {
-      return store.dispatch(openAuth());
-    } else {
-      return next(action);
-    }
-  }
-}
 
 const persistedState = loadState();
 const middleware = applyMiddleware(
@@ -30,7 +17,6 @@ const middleware = applyMiddleware(
   thunk,
   routerMiddleware(history),
   createLogger(),
-  authMiddleware(['VOTE_UP_PENDING', 'VOTE_DOWN_PENDING', 'MY_VOTES_PENDING'])
 );
 
 export default createStore(reducer, persistedState, middleware);
