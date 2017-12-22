a59ecb7a15406e6d7c6bb86671149b5cfb4743bf
atamano <atamano@student.42.fr>
2017-12-22 17:04:38 +0100

    Update circle deployment scripts

---
+++ scripts/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Google Cloud authentication
+echo $GCLOUD_SERVICE_KEY | base64 --decode > $HOME/gcloud-service-key.json && cp $HOME/gcloud-service-key.json gcloud-service-key.json
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update
+sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file $HOME/gcloud-service-key.json
+sudo /opt/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT
+
+# Build the image
+yarn build
+docker build -t eu.gcr.io/$GCLOUD_PROJECT/gov-timeline:$CIRCLE_SHA1 .
+
+# Push the images to Google Cloud
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push eu.gcr.io/$GCLOUD_PROJECT/gov-timeline:$CIRCLE_SHA1
