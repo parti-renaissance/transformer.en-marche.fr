2fd15d0040b79a4ec2121fae294e26f00e93bb90
Brian Whitton <me@brianwhitton.com>
2017-12-29 14:42:23 -0500

    transition in width of progress meters

--- src/js/components/progress-meter.js
+++ src/js/components/progress-meter.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition';
 
 import '../../scss/progress-meter.css';
 
-const ProgressBar = ({ style }) => <div className="progress-bar" style={style} />
+const duration = 225;
+
+const ProgressBar = ({ width }) => (
+  <Transition in={true} timeout={duration} appear={true}>
+    {(state) => (
+      <div className="progress-bar" style={{
+        transition: `width ${duration}ms ease-out 500ms`,
+        width: state === 'entering' ? '0%' : width
+      }} />
+    )}
+  </Transition>
+)
 
 export default class ProgressMeter extends Component {
   render() {
     let { current, total, children, className, reverse } = this.props
     
-    let progressBarStyle = {
-      width: `${(current / total) * 100}%`,
-    }
+    let targetWidth = `${(current / total) * 100}%`;
     if (reverse) {
-      progressBarStyle.width = (100 - parseInt(progressBarStyle.width, 10)) + '%';
+      targetWidth = (100 - parseInt(targetWidth, 10)) + '%';
     }
     
     return (
       <div className={`progress-meter ${className || ''}`}>
         <span className="progress-meter__label">{children}</span>
-        <ProgressBar style={progressBarStyle} />
+        <ProgressBar width={targetWidth} />
       </div>
     )
   }
