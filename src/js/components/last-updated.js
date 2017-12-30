f8cdd122931bdf1af19559bdf6cb12957833d4b6
Brian Whitton <me@brianwhitton.com>
2017-12-30 18:12:12 -0500

    this only needs to be rendered once

--- src/js/components/last-updated.js
+++ src/js/components/last-updated.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { map } from 'lodash';
 import moment from 'moment-timezone';
 import { connect } from 'react-redux';
 
-const LastUpdated = ({ measures: { measures }, className }) => {
-  let dates = map(measures, 'updatedAt');
-  dates.sort();
-  let diff = moment().diff(dates[0], 'days');
-  let unit = 'jours';
-  if (diff < 1) {
-    diff = moment().diff(dates[0], 'hours');
-    unit = 'hours';
+class LastUpdated extends Component {
+  shouldComponentUpdate(nextProps) {
+    return nextProps.measures.items.length !== this.props.measures.items.length;
   }
-  return <small className={className}>Dernière mise à jour il y a {diff} {unit}.</small>
-};
+  
+  render() {
+    let { measures: { measures }, className } = this.props;
+    let dates = map(measures, 'updatedAt');
+    dates.sort();
+    let diff = moment().diff(dates[0], 'days');
+    let unit = 'jours';
+    if (diff < 1) {
+      diff = moment().diff(dates[0], 'hours');
+      unit = 'hours';
+    }
+    return <small className={className}>Dernière mise à jour il y a {diff} {unit}.</small>
+  }
+
+}
 
 export default connect(state => ({measures: state.measures}))(LastUpdated);
