ec456f54cb3da776d55ab10c9f6adfdc2596d3fa
Brian Whitton <me@brianwhitton.com>
2018-01-04 11:10:37 -0500

    adds clear token actions and reducer case

--- src/js/reducers/auth-reducer.js
+++ src/js/reducers/auth-reducer.js
@@ -1,10 +1,13 @@
 import {
   OPEN_AUTH,
   CLOSE_AUTH,
-  SET_TOKEN
+  SET_TOKEN,
+  CLEAR_TOKEN
  } from '../actions/auth-actions';
+ 
+import { VOTES, MY_VOTES, VOTE_UP, VOTE_DOWN } from '../actions/vote-actions';
 
-export default function authReducer(state = { token: '', openModal: false }, action) {
+export default function authReducer(state = { token: '', openModal: false, fetchedToken: false }, action) {
   switch(action.type) {
     case OPEN_AUTH:
       return {...state, openModal: true};
@@ -15,7 +18,13 @@ export default function authReducer(state = { token: '', openModal: false }, act
     case `${SET_TOKEN}_PENDING`:
       return {...state, fetchingToken: true};
     case `${SET_TOKEN}_REJECTED`:
-      return {...state, fetchingToken: false, error: action.payload};
+      return {
+        ...state,
+        token: '',
+        fetchedToken: false,
+        fetchingToken: false,
+        error: action.payload,
+      };
     case `${SET_TOKEN}_FULFILLED`:
       return {
         ...state,
@@ -23,6 +32,13 @@ export default function authReducer(state = { token: '', openModal: false }, act
         fetchedToken: true,
         token: action.payload.access_token
       };
+    
+    case `${VOTES}_REJECTED`:
+    case `${MY_VOTES}_REJECTED`:
+    case `${VOTE_UP}_REJECTED`:
+    case `${VOTE_DOWN}_REJECTED`:
+    case CLEAR_TOKEN:
+      return {...state, token: '', fetchedToken: false};
 
     default:
       return state;
