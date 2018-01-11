a5e47ce841c6cf4d3685f182b9fe34ecb587ca6b
Brian Whitton <me@brianwhitton.com>
2018-01-11 10:54:19 -0500

    initial translate dropdown

--- src/js/actions/translate-actions.js
+++ src/js/actions/translate-actions.js
@@ -1,6 +1,17 @@
+import { push } from 'react-router-redux'
+
 export const SET_LANGUAGE = 'SET_LANGUAGE';
 
 export const setLanguage = language => ({
   type: SET_LANGUAGE,
   payload: language
 });
+
+export const setLocale = (locale, {pathname, search}) => dispatch => {
+  let [, path] = pathname.slice(1).split('/');
+  if (path && search) {
+    path += search;
+  }
+  dispatch(push(`/${locale}${path ? `/${path}` : ''}`));
+  dispatch(setLanguage(locale));
+}
