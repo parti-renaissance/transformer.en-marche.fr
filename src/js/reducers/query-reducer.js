10f0592ebe3b09fbc9744e8fc6298462a9fd4233
Brian Whitton <me@brianwhitton.com>
2018-01-13 17:44:34 -0500

    simplify searchState

--- src/js/reducers/query-reducer.js
+++ src/js/reducers/query-reducer.js
@@ -1,31 +1,18 @@
 import { DO_QUERY, RESET_PARAMS, QUERY } from '../actions/search-actions';
 
-export default function queryReducer(state = {
-  searchState: {}
-}, action) {
+export default function queryReducer(state = '', action) {
 
   switch(action.type) {
     case DO_QUERY:
       if (action.payload) {
-        return {
-          ...state,
-          searchState: {
-            query: action.payload
-          }
-        }
+        return action.payload;
       } else {
-        return {
-          ...state,
-          searchState: {}
-        }
+        return state;
       }
       
     case `RESET_${QUERY}`:
     case RESET_PARAMS:
-      return {
-        ...state,
-        searchState: {}
-      };
+      return '';
   
     default:
       return state;
