f8c4e0a6a191f77ba569d1cc98cc339f48fbb7a2
Marine Boudeau <m14868@gmail.com>
2018-01-14 12:31:49 +0100

    Fix modal on mobile

--- src/js/components/about-modal.js
+++ src/js/components/about-modal.js
@@ -38,7 +38,10 @@ const AboutModal = ({ isOpen, closeModal }) =>
     </div>
 
     <button className="about-modal__close-button" onClick={closeModal}>
-      Fermer <span>X</span>
+      Fermer
+      <span>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+      </span>
     </button>
   </Modal>
 
