20957ae9d1e63a00cfa4372cd67cb8856f5d9a11
Brian Whitton <me@brianwhitton.com>
2018-01-13 19:07:07 -0500

    send pageviews
    according to this write up:
    https://developers.google.com/analytics/devguides/collection/gtagjs/single-page-applications

--- src/js/store.js
+++ src/js/store.js
@@ -1,3 +1,4 @@
+/* global gtag */
 import { applyMiddleware, createStore, compose } from 'redux';
 
 import { routerMiddleware } from 'react-router-redux';
@@ -11,6 +12,9 @@ import reducer from './reducers';
 const rootPath = process.env.REACT_APP_ROOT_PATH || ''; // for access assets when running on a nested path, i.e. github pages
 
 export const history = createHistory({ basename: rootPath });
+history.listen(location => gtag('config', 'UA-72646335-2', {
+  page_path: `${location.pathname}${location.search}${location.hash}`
+}))
 
 const persistedState = loadState();
 const middlewares = [
