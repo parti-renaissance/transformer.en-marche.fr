4fae043e46fb4a3b37db81508567a8cca7b4ee40
Brian Whitton <me@brianwhitton.com>
2018-01-17 12:36:40 -0500

    add replaceState option to setLocale

--- src/js/actions/translate-actions.js
+++ src/js/actions/translate-actions.js
@@ -1,4 +1,4 @@
-import { push } from 'react-router-redux'
+import { push, replace } from 'react-router-redux'
 
 export const SET_LANGUAGE = 'SET_LANGUAGE';
 
@@ -7,11 +7,15 @@ export const setLanguage = language => ({
   payload: language
 });
 
-export const setLocale = (locale, {pathname, search}) => dispatch => {
+export const setLocale = (locale, {pathname, search}, replaceState = false) => dispatch => {
   let [, path] = pathname.slice(1).split('/');
   if (path && search) {
     path += search;
   }
-  dispatch(push(`/${locale}${path ? `/${path}` : ''}`));
+  if (replaceState) {
+    dispatch(replace(`/${locale}${path ? `/${path}` : ''}`));
+  } else {
+    dispatch(push(`/${locale}${path ? `/${path}` : ''}`));
+  }
   dispatch(setLanguage(locale));
 }
