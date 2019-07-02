8c8f75af56498ba63e4e3c5b1f43060667df0cd3
Brian Whitton <me@brianwhitton.com>
2019-07-02 18:18:12 -0400

    try changing to google image and put all the commands in the script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -e
 
+TAG=${CIRCLE_TAG:-$CIRCLE_SHA1}
+
 # Google Cloud authentication
 echo $GCLOUD_SERVICE_KEY | base64 --decode > $HOME/gcloud-service-key.json && cp $HOME/gcloud-service-key.json gcloud-service-key.json
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update
-sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file $HOME/gcloud-service-key.json
-sudo /opt/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT
+gcloud --quiet components update
+gcloud auth activate-service-account --key-file $HOME/gcloud-service-key.json
+gcloud config set project $GCLOUD_PROJECT
 
 # Build the image
 yarn build
-docker build -t eu.gcr.io/$GCLOUD_PROJECT/gov-timeline:${1:-$CIRCLE_SHA1} .
+docker build -t eu.gcr.io/$GCLOUD_PROJECT/gov-timeline:$TAG .
 
 # Push the images to Google Cloud
-sudo /opt/google-cloud-sdk/bin/gcloud docker -- push eu.gcr.io/$GCLOUD_PROJECT/gov-timeline:${1:-$CIRCLE_SHA1}
+gcloud docker -- push eu.gcr.io/$GCLOUD_PROJECT/gov-timeline:$TAG
+
+gcloud container clusters get-credentials $PRODUCTION_GCLOUD_CLUSTER --project $GCLOUD_PROJECT --zone europe-west1-d
+GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcloud-service-key.json kubectl set image deployment/prod-gov-timeline gov-timeline=eu.gcr.io/$GCLOUD_PROJECT/gov-timeline:$TAG
